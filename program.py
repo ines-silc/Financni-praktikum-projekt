0c61863d84b94e97b54be1bb797f907f7bc8d057
ines-silc <ines.silc@gmail.com>
2020-01-02 15:37:20 +0100

    Dokončan uvod

--- program.py
+++ program.py
@@ -1,15 +1,16 @@
 import time #Za primerjavo časovne zahtevnosti
 import networkx as nx #Za definiranje grafov
 import matplotlib.pyplot as mpl #Za risanje grafov
+import random
 
 g = nx.MultiGraph()
-niz = 'abcdefgh'
-g.add_nodes_from(niz)
+sez = [1, 2, 3, 4, 5, 6, 7, 8]
+g.add_nodes_from(sez)
 
 a = 0
-for i in niz:
+for i in sez:
     a += 1
-    for j in niz:
+    for j in sez:
         a += 1
         if i == j:
             pass
@@ -33,3 +34,25 @@ h = nx.complete_graph(8)
 ## pot.size(weight = None) == pot.order()
 
 ##Dolžina poti: pot.size()
+
+##Pretvorimo graf v matriko, za izvajanje algoritma:
+
+gm = nx.attr_matrix(g, edge_attr = 'weight')[0]
+
+##Pretvorimo matriko v graf:
+
+def pretvori_matriko_v_graf(matrika):
+    slovar = dict()
+    for i in range(len(matrika[0])):
+        for j in range(len(matrika)):
+            if i == j:
+                pass
+            else:
+                slovar[i] = {j: {'weight' : matrika[i][j]}}
+    graf = nx.from_dict_of_dicts(slovar)
+    return graf
+
+
+
+
+
